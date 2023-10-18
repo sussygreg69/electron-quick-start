458b82b1300772274363943b99137b90e960fcf7
sussygreg69 <gregormalberg16@gmail.com>
2023-10-18 15:13:06 +0300

    g

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ function createWindow () {
 }
 
 // This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
+// initialization and is ready to creagfghfgte browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
   createWindow()
